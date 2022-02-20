b13ecefdc531de866a9042136bbd6efa255bcbb5
cerlaw <bossllcwn@gmail.com>
2022-02-20 17:27:07 +0800

    Add files via upload

--- app/src/main/java/com/example/testdemo1/AlignTextView.java
+++ app/src/main/java/com/example/testdemo1/AlignTextView.java
@@ -1,10 +1,12 @@
-package com.example.testdemo1;
+package com.example.myapplication;
 
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.text.Layout;
 import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 
 import androidx.annotation.Nullable;
@@ -36,11 +38,17 @@ public class AlignTextView extends AppCompatTextView {
         init(context, attrs);
     }
 
+    private final String dot = "...";
+    private final TextPaint textPaint = new TextPaint();
+    private float dotWidth;
+
     private void init(Context context, AttributeSet attrs) {
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.AlignTextView);
         alignOnlyOneLine = typedArray.getBoolean(R.styleable.AlignTextView_alignOnlyOneLine, false);
         typedArray.recycle();
         setTextColor(getCurrentTextColor());
+        textPaint.setTextSize(getTextSize());
+        dotWidth = textPaint.measureText(dot);
     }
 
     @Override
@@ -62,12 +70,17 @@ public class AlignTextView extends AppCompatTextView {
             int lineBaseline = layout.getLineBaseline(i) + getPaddingTop();
             int lineStart = layout.getLineStart(i);
             int lineEnd = layout.getLineEnd(i);
-            if (alignOnlyOneLine && layout.getLineCount() == 1) { // 只有一行
+            if (alignOnlyOneLine && layout.getLineCount() == 1) {
+                // 只有一行
                 String line = text.substring(lineStart, lineEnd);
                 float width = StaticLayout.getDesiredWidth(text, lineStart, lineEnd, getPaint());
                 this.drawScaledText(canvas, line, lineBaseline, width);
-            } else if (i == layout.getLineCount() - 1) { // 最后一行
-                canvas.drawText(text.substring(lineStart), getPaddingLeft(), lineBaseline, getPaint());
+            } else if (i == layout.getLineCount() - 1) {
+                // 最后一行
+                //int ellipsisStart = layout.get(i);
+                //String s = text.substring(lineStart, ellipsisStart);
+                String display = getLastRowString(text, lineStart);
+                canvas.drawText(display, getPaddingLeft(), lineBaseline, getPaint());
                 break;
             } else { //中间行
                 String line = text.substring(lineStart, lineEnd);
@@ -76,6 +89,19 @@ public class AlignTextView extends AppCompatTextView {
             }
         }
 
+    }
+
+    private String getLastRowString(String text, int lineStart) {
+        String display = text.substring(lineStart);
+        if (textPaint.measureText(display) > getWidth()) {
+            if (getEllipsize() == TextUtils.TruncateAt.END) {
+                while (textPaint.measureText(display) >= (getWidth() - dotWidth)) {
+                    display = display.substring(0, display.length() - 1);
+                }
+                display += dot;
+            }
+        }
+        return display;
     }
 
     private void drawScaledText(Canvas canvas, String line, float baseLineY, float lineWidth) {
